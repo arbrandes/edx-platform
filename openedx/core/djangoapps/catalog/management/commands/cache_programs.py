0d1b72a334aa3f7c160d71f0bc55a18d11ddcccc
Renzo Lucioni <renzo@lucioni.xyz>
2017-05-03 15:02:40 -0400

    fixup! Add management command for caching program data

--- openedx/core/djangoapps/catalog/management/commands/cache_programs.py
+++ openedx/core/djangoapps/catalog/management/commands/cache_programs.py
@@ -6,7 +6,6 @@ from django.core.management import BaseCommand
 
 from openedx.core.djangoapps.catalog.models import CatalogIntegration
 from openedx.core.djangoapps.catalog.utils import create_catalog_api_client
-from openedx.core.lib.edx_api_utils import traverse_pagination
 
 
 logger = logging.getLogger(__name__)
@@ -19,11 +18,11 @@ class Command(BaseCommand):
     """Management command used to cache program data.
 
     This command requests every available program from the discovery
-    service, caching each in its own cache entry with an indefinite expiration.
+    service, writing each to its own cache entry with an indefinite expiration.
     It is meant to be run on a scheduled basis and should be the only code
     updating these cache entries.
     """
-    help = 'Backpopulate missing program credentials.'
+    help = 'Cache program data.'
 
     def add_arguments(self, parser):
         parser.add_argument(
@@ -49,14 +48,11 @@ class Command(BaseCommand):
 
         querystring = {
             'exclude_utm': 1,
-            'status': ('active', 'retired',),
+            'status': ('active', 'retired'),
+            'uuids_only': 1,
         }
 
         endpoint = getattr(client, 'programs')
-        response = endpoint.get(**querystring)
+        uuids = endpoint.get(**querystring)
 
-        results = traverse_pagination(response, endpoint, querystring, [])
-        # TODO: Prefix these keys.
-        programs = {result['uuid']: result for result in results}
-
-        cache.set(PROGRAM_UUIDS_KEY, list(programs.keys()), None)
+        cache.set(PROGRAM_UUIDS_KEY, uuids, None)
